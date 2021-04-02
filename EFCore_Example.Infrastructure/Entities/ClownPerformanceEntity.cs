9bcdba97759fc8eb2af7f078fafe41f17c09968a
Mikhail Ilyushin <mishamine26@gmail.com>
2021-04-02 23:11:26 +0300

    добавлены комментарии и доп. операции

--- EFCore_Example.Infrastructure/Entities/ClownPerformanceEntity.cs
+++ EFCore_Example.Infrastructure/Entities/ClownPerformanceEntity.cs
@@ -1,6 +1,5 @@
 ﻿#region Using namespaces
 
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
--- EFCore_Example.UI/Program.cs
+++ EFCore_Example.UI/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EFCore_Example.Infrastructure;
 using EFCore_Example.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 #endregion
 
@@ -16,18 +17,67 @@ namespace EFCore_Example.UI
 
         private static async Task Main(string[] args)
         {
-            var clowns = Enumerable.Range(0, 100).Select(x => CreateClown()).ToArray();
-            var circuses = Enumerable.Range(0, 10).Select(x => CreateCircus()).ToArray();
-            var events = Enumerable.Range(0, 30).Select(x => CreateCircusEvent(circuses));
-            var performances = Enumerable.Range(0, 200).Select(x => CreateClownPerformance(clowns, circuses));
+            await using var db = new ClownsContext();
 
-            await using var context = new ClownsContext();
-            await context.Clowns.AddRangeAsync(clowns);
-            await context.Circuses.AddRangeAsync(circuses);
-            await context.Events.AddRangeAsync(events);
-            await context.ClownPerformances.AddRangeAsync(performances);
-            await context.SaveChangesAsync();
+            // пройтись по всем сущностям и поменять значения
+            await db.Clowns.ForEachAsync(x => x.Name = "баран");
+
+            // сохранение текущего состояния контекста
+            await db.SaveChangesAsync();
+
+            // фильтрация данных
+            var clowns2 = await db.Clowns
+                                  .Where(x => x.Name == "баран")
+                                  .ToListAsync();
+
+            // вывод всех клоунов с именем "баран"
+            foreach (var item in clowns2)
+                Console.WriteLine(item.Name);
+
+            // получение самого первого клоуна
+            var firstClown = await db.Clowns.FirstOrDefaultAsync();
+
+            // устанавливаем новое значение свойства
+            firstClown.Age = 10;
+
+            await db.SaveChangesAsync();
+
+            var clownToRemove = db.Clowns.ElementAtOrDefault(10);
+
+            // удаление
+            if (clownToRemove is not null)
+            {
+                db.Clowns.Remove(clownToRemove);
+                await db.SaveChangesAsync();
+            }
+
+            #region Generate and fill
+
+            //var clowns = Enumerable.Range(0, 100)
+            //                       .Select(x => CreateClown())
+            //                       .ToArray();
+
+            //var circuses = Enumerable.Range(0, 10)
+            //                         .Select(x => CreateCircus())
+            //                         .ToArray();
+
+            //var events = Enumerable.Range(0, 30)
+            //                       .Select(x => CreateCircusEvent(circuses));
+
+            //var performances = Enumerable.Range(0, 200)
+            //                             .Select(x => CreateClownPerformance(clowns, circuses));
+
+            //await using var context = new ClownsContext();
+            //await context.Clowns.AddRangeAsync(clowns);
+            //await context.Circuses.AddRangeAsync(circuses);
+            //await context.Events.AddRangeAsync(events);
+            //await context.ClownPerformances.AddRangeAsync(performances);
+            //await context.SaveChangesAsync();
+
+            #endregion
         }
+
+        #region Generate section
 
         private static ClownEntity CreateClown() =>
             new()
@@ -39,9 +89,17 @@ namespace EFCore_Example.UI
         private static ClownPerformanceEntity CreateClownPerformance(ClownEntity[] clowns, CircusEntity[] circuses) =>
             new()
             {
-                Circus = circuses[Random.Next(0, circuses.Length)],
-                Clown = clowns[Random.Next(0, clowns.Length)],
-                Title = Guid.NewGuid().ToString().Substring(0, 11)
+                Circus =
+                    circuses[Random.Next(0,
+                                         circuses
+                                             .Length)],
+                Clown =
+                    clowns[Random.Next(0,
+                                       clowns
+                                           .Length)],
+                Title = Guid.NewGuid()
+                            .ToString()
+                            .Substring(0, 11)
             };
 
         private static CircusEntity CreateCircus() =>
@@ -58,5 +116,7 @@ namespace EFCore_Example.UI
                 End = DateTime.Now.AddHours(Random.Next(1, 100)),
                 Title = Guid.NewGuid().ToString().Substring(0, 6)
             };
+
+        #endregion
     }
 }
